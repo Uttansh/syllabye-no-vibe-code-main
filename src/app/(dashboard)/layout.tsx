c94b155d0dee342386f4153ec5ca0034e6c79ca3
Uttansh <uttansh4321@gmail.com>
2026-03-09 01:35:07 -0400

    Sidebar responsiveness update

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export default async function DashboardLayout({
   const courses = await getCourses();
 
   return (
-    <div className="grid grid-cols-[0_1fr] xl:grid-cols-[1fr_5fr] gap-4 h-screen bg-neutral-50 dark:bg-neutral-800">
+    <div className="grid grid-cols-[0_1fr] xl:grid-cols-[280px_1fr] gap-4 h-screen bg-neutral-50 dark:bg-neutral-800">
       <DashboardSidebar courses={courses} />
       <main className="min-w-0 pt-16 xl:pt-4 h-full overflow-hidden p-4 pl-0">
         {children}
